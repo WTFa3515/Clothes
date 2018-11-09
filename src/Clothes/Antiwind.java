c27bc19e4b5c0f2ab8c1c8ea81c63dadf4d3d466
WTFa3515 <sinba840720@gmail.com>
2018-11-09 21:52:18 +0800

    modify Raincoat

---
+++ src/Clothes/Antiwind.java
@@ -0,0 +1,5 @@
+package Clothes;
+
+public interface Antiwind {
+	public abstract void antiwind(); 
+}
---
+++ src/Clothes/Raincoat.java
@@ -0,0 +1,30 @@
+package Clothes;
+
+public class Raincoat extends Clothes implements Antiwind {
+
+	public Raincoat() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Raincoat(String size) {
+		super(size);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Raincoat(String size, String color) {
+		super(size, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Raincoat(String size, String color, int price) {
+		super(size, color, price);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void antiwind() {
+		// TODO Auto-generated method stub
+		System.out.println("¨¾­·«B¦ç");
+	}
+
+}
